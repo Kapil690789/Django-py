44274692b3398815d195a3bfeb79bae571f6dd1a
Kapil Sharma <kapil19092003@gmail.com>
2025-05-17 19:35:21 +0530

    updated code

--- kapilaurDjango/kapil/urls.py
+++ kapilaurDjango/kapil/urls.py
@@ -4,4 +4,6 @@ from . import views
 # localhost:8000/kapil
 urlpatterns = [
     path('', views.all_chai, name='all_kapil'),
+    path('<int:chai_id>/', views.chai_detail, name='chai_detail'),
+
 ]
--- kapilaurDjango/kapil/views.py
+++ kapilaurDjango/kapil/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import ChaiVarity
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def all_chai(request):
-    return render(request, 'kapil/all_kapil.html')
+    chais = ChaiVarity.objects.all()
+    return render(request, 'kapil/all_kapil.html', {'chais': chais})
+
+def chai_detail(request, chai_id):
+    chai = get_object_or_404(ChaiVarity, pk=chai_id)
+    return render(request, 'kapil/chai_detail.html', {'chai':chai})
